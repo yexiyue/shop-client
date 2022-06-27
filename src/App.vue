899196feeeb36a70b9ebb90a95d02e58a04f2884
yeshifu <yexiyue666@qq.com>
2022-06-27 09:39:13 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+onLaunch(() => {
+  console.log("App Launch");
+});
+onShow(() => {
+  console.log("App Show");
+});
+onHide(() => {
+  console.log("App Hide");
+});
+</script>
+<style>
+/* 全局公共样式 */
+@import url(@/common/uni.css);
+@import url(@/common/iconfont.css);
+@import url(@/common/common.css);
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="index">
+      <text class="f-active-color">文字</text>
+      <view class="iconfont icon-sousuo">test </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
